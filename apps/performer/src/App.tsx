13f2292ba7958ba29cbfa988982523aa537c4671
bryan-goodwork <bryan@goodwork.sg>
2022-09-29 11:14:13 +0800

    added craco to enable loading of uncompiled packages within monorepo

--- apps/performer/src/App.tsx
+++ apps/performer/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
-// import Container from "ui/Container";
+import Container from "ui/Container";
 import { Button } from "ui";
 
 function App() {
-  return <Button />;
+  return (
+    <Container>
+      <Button />
+    </Container>
+  );
 }
 
 export default App;

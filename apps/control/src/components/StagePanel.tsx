329b8eed5e77b365976f661c367c410b4c43636c
bryangoodwork <bryan@goodwork.sg>
2022-11-09 16:38:41 +0800

    feat: video layouts

--- apps/control/src/components/StagePanel.tsx
+++ apps/control/src/components/StagePanel.tsx
@@ -1,7 +1,6 @@
 import { Participant } from "livekit-client";
 import React, { Dispatch, SetStateAction } from "react";
 import { Sad } from "react-ionicons";
-import Heading from "ui/Texts/Heading";
 import Text from "ui/Texts/Text";
 import { styled } from "ui/theme/theme";
 import { PanelStates } from "../../../../types/stageStates";
@@ -9,15 +8,141 @@ import AudioMixCard from "./AudioMixCard";
 
 import { Root, Viewport, Scrollbar } from "@radix-ui/react-scroll-area";
 
+const videoLayouts = [
+  {
+    image: "images/video-layouts/layout-a.png",
+    name: "A",
+  },
+  {
+    image: "images/video-layouts/layout-b.png",
+    name: "B",
+  },
+  {
+    image: "images/video-layouts/layout-c.png",
+    name: "C",
+  },
+  {
+    image: "images/video-layouts/layout-d.png",
+    name: "D",
+  },
+  {
+    image: "images/video-layouts/layout-e.png",
+    name: "E",
+  },
+  {
+    image: "images/video-layouts/layout-f.png",
+    name: "F",
+  },
+  {
+    image: "images/video-layouts/layout-g.png",
+    name: "G",
+  },
+  {
+    image: "images/video-layouts/layout-h.png",
+    name: "H",
+  },
+  {
+    image: "images/video-layouts/layout-i.png",
+    name: "I",
+  },
+  {
+    image: "images/video-layouts/layout-j.png",
+    name: "J",
+  },
+  {
+    image: "images/video-layouts/layout-k.png",
+    name: "K",
+  },
+  {
+    image: "images/video-layouts/layout-l.png",
+    name: "L",
+  },
+];
+
 const StyledPanel = styled("div", {
   height: "100%",
   width: "100%",
   color: "white",
-  display: "grid",
-  gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))",
   gridGap: "$md",
   boxSizing: "border-box",
   paddingRight: "$lg",
+
+  variants: {
+    variant: {
+      audio: {
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))",
+      },
+      video: {
+        display: "flex",
+        flexDirection: "column",
+        ".participants": { display: "flex", width: "100%" },
+        ".video-box": {
+          display: "flex",
+          boxSizing: "border-box",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          border: "2px solid $brand",
+          backgroundColor: "$background",
+          padding: "$md $md $xs $md",
+          gap: "$sm",
+          ".layouts": {
+            gap: "$xs",
+            boxSizing: "border-box",
+            display: "flex",
+            justifyContent: "space-around",
+            padding: "0 $lg",
+            alignItems: "center",
+            ".item": {
+              appearance: "none",
+              outline: "none",
+              background: "$background",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "$2xs",
+              border: "1px solid $brand",
+              width: "50px",
+              img: {
+                objectFit: "contain",
+                width: "100%",
+              },
+              "&:hover": {
+                cursor: "pointer",
+                background: "$brand",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+const VideoGrid = styled("div", {
+  display: "grid",
+  width: "100%",
+  height: "100%",
+  border: "2px solid $brand",
+  variants: {
+    layout: {
+      1: {
+        // gridTemplateColumns:
+      },
+      2: {},
+      3: {},
+      4: {},
+      5: {},
+      6: {},
+      7: {},
+      8: {},
+      9: {},
+      10: {},
+      11: {},
+      12: {},
+    },
+  },
 });
 
 const StyledEmptyState = styled("div", {
@@ -48,6 +173,29 @@ const StyledViewport = styled(Viewport, {
   width: "100%",
 });
 
+const ParticipantLayout = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  boxSizing: "border-box",
+  width: "50px",
+  gap: "$2xs",
+  "> div": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    boxSizing: "border-box",
+    border: "1px solid $brand",
+    background: "$background",
+    padding: "$2xs",
+    img: {
+      width: "100%",
+      objectFit: "contain",
+    },
+  },
+});
+
 export default function StagePanel({
   activePanel,
   setActivePanel,
@@ -71,7 +219,7 @@ export default function StagePanel({
       return (
         <StyledRoot>
           <StyledViewport>
-            <StyledPanel>
+            <StyledPanel variant="audio">
               {participants.map((p: any) => {
                 return (
                   <AudioMixCard
@@ -88,8 +236,36 @@ export default function StagePanel({
       );
   } else
     return (
-      <StyledPanel>
-        <Heading>Video Panel</Heading>
+      <StyledPanel variant="video">
+        <div className="video-box">
+          <VideoGrid layout={1}></VideoGrid>
+          <div className="layouts">
+            {videoLayouts.map((l) => {
+              return (
+                <button key={l.name} className={`item layout-${l.name}`}>
+                  <img src={l.image} alt={`Layout ${l.name}`} />
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="participants">
+          {participants.map((p: any) => {
+            if (JSON.parse(p.metadata).type !== "CONTROL") {
+              return (
+                <ParticipantLayout>
+                  <div>
+                    <img
+                      src={videoLayouts[0].image}
+                      alt={videoLayouts[0].name}
+                    />
+                  </div>
+                  <Text size="xs">{p.identity}</Text>
+                </ParticipantLayout>
+              );
+            }
+          })}
+        </div>
       </StyledPanel>
     );
 }

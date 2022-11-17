985e36dde024d1fab7acd851567b6d782cd2f158
siahgoodwork <83395882+siahgoodwork@users.noreply.github.com>
2022-11-17 10:57:15 +0900

    feat: initial commit for output

---
+++ apps/output/src/App.tsx
@@ -0,0 +1,162 @@
+import { LegacyRef, useEffect, useRef, useState } from "react";
+import { useParticipant, useRoom } from "@livekit/react-core";
+import queryString from "query-string";
+import {
+  Participant,
+  ParticipantEvent,
+  RemoteTrackPublication,
+  RoomEvent,
+  Track,
+} from "livekit-client";
+import { ServerUpdate } from "@thegoodwork/ximi-types/src/room";
+
+const decoder = new TextDecoder();
+
+function App() {
+  const { participants, connect } = useRoom();
+  const [target, setTarget] = useState<string>();
+  const [withAudio, setWithAudio] = useState<boolean>(false);
+  const thisParticipant = participants.find((p) => p.identity === target);
+
+  useEffect(() => {
+    // get param upon load
+    const query = queryString.parse(window.location.search);
+    setWithAudio(() => query.withAudio === "true");
+
+    const joinPayload = {
+      participant_type: "OUTPUT",
+      room_name: query.room,
+      performer_target: query.target,
+      passcode: query.passcode,
+    };
+
+    setTarget(query.target as string);
+
+    fetch(`${process.env.REACT_APP_SERVER_HOST}/rooms/validate-passcode`, {
+      method: "POST",
+      body: JSON.stringify(joinPayload),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      response.json().then((body) => {
+        const token = body.data;
+        connect(`${process.env.REACT_APP_LIVEKIT_HOST}`, token, {
+          autoSubscribe: false,
+        }).then((room) => {
+          if (!room) {
+            return;
+          }
+          room.on(RoomEvent.DataReceived, (payload) => {
+            const string = decoder.decode(payload);
+            try {
+              const json = JSON.parse(string) as ServerUpdate;
+
+              console.log(json);
+              if (!json.type) {
+                return;
+              }
+              if (json.type === "room-update") {
+                console.log(json);
+              }
+            } catch (err) {
+              console.log(err);
+              return;
+            }
+          });
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div id="App">
+      {thisParticipant && (
+        <>
+          {withAudio && <PerformerAudio performer={thisParticipant} />}
+          <PerformerVideo performer={thisParticipant} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+const PerformerAudio = ({ performer }: { performer: Participant }) => {
+  const [start, setStart] = useState<boolean>(false);
+  const p = useParticipant(performer);
+  const audioRef = useRef<HTMLAudioElement>();
+  const firstAudioTrack = p.publications.find(
+    (pub) => pub.kind === Track.Kind.Audio
+  ) as RemoteTrackPublication | undefined;
+  useEffect(() => {
+    if (firstAudioTrack) {
+      if (!firstAudioTrack.isSubscribed) {
+        firstAudioTrack.setSubscribed(true);
+      } else {
+        firstAudioTrack.setEnabled(true);
+        if (audioRef.current) {
+          firstAudioTrack.track?.attach(audioRef.current);
+        }
+      }
+    }
+  }, [firstAudioTrack, firstAudioTrack?.isSubscribed, start]);
+
+  return (
+    <>
+      {!start && (
+        <button
+          onClick={() => {
+            setStart(!start);
+          }}
+          style={{
+            position: "fixed",
+            bottom: "10px",
+            right: "10px",
+            zIndex: 5,
+            background: "transparent",
+            color: "white",
+            appearance: "none",
+            border: 0,
+          }}
+        >
+          Start
+        </button>
+      )}
+      <audio ref={audioRef as LegacyRef<HTMLAudioElement>} />
+    </>
+  );
+};
+
+const PerformerVideo = ({ performer }: { performer: Participant }) => {
+  const p = useParticipant(performer);
+  const videoRef = useRef<HTMLVideoElement>();
+  const firstVideoTrack = p.publications.find(
+    (pub) => pub.kind === Track.Kind.Video
+  ) as RemoteTrackPublication | undefined;
+  useEffect(() => {
+    if (firstVideoTrack) {
+      if (!firstVideoTrack.isSubscribed) {
+        firstVideoTrack.setSubscribed(true);
+      } else {
+        firstVideoTrack.setEnabled(true);
+        if (videoRef.current) {
+          firstVideoTrack.track?.attach(videoRef.current);
+        }
+      }
+    }
+  }, [firstVideoTrack, firstVideoTrack?.isSubscribed]);
+
+  return (
+    <>
+      <video
+        ref={videoRef as LegacyRef<HTMLVideoElement>}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "fixed",
+          objectFit: "cover",
+        }}
+      />
+    </>
+  );
+};
---
+++ apps/output/src/screens/RoomsList.tsx
@@ -0,0 +1,158 @@
+import IconButton from "ui/Buttons/IconButton";
+import ListButton from "ui/Buttons/ListButton";
+import { styled } from "ui/theme/theme";
+import React, { Dispatch } from "react";
+import Heading from "ui/Texts/Heading";
+import { SyncOutline, SadOutline } from "react-ionicons";
+import { Room, UpdateStateActions } from "../../../../types/performerStates";
+import Text from "ui/Texts/Text";
+import Icon from "ui/Texts/Icon";
+import useSWR, { SWRConfiguration } from "swr";
+
+const fetcher = (args: any) => fetch(args).then((res) => res.json());
+const options: SWRConfiguration = {
+  revalidateOnFocus: true,
+  revalidateIfStale: true,
+  shouldRetryOnError: false,
+};
+
+const HeadingBox = styled("div", {
+  width: "auto",
+  maxWidth: "600px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "$lg",
+  position: "relative",
+  "> .refresh-icon-container": {
+    position: "absolute",
+    left: "calc( 100% + 20px )",
+  },
+});
+
+const EmptyState = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  minWidth: "500px",
+  width: "100%",
+  maxWidth: "600px",
+  marginTop: "$lg",
+  gap: "$sm",
+});
+
+const List = styled("ul", {
+  display: "flex",
+  flexDirection: "column",
+  minWidth: "500px",
+  width: "100%",
+  maxWidth: "600px",
+  paddingLeft: "0",
+  listStyle: "none",
+
+  "@base": {
+    gap: "$xs",
+  },
+  "@md": {
+    gap: "$sm",
+  },
+});
+
+export default function RoomsList({
+  updateState,
+}: {
+  updateState: Dispatch<UpdateStateActions>;
+}) {
+  const {
+    data,
+    mutate,
+    isValidating: isRefreshing,
+    error,
+  } = useSWR(
+    `${process.env.REACT_APP_SERVER_HOST}/rooms/list`,
+    fetcher,
+    options
+  );
+
+  const rooms: Room[] = data?.data || [];
+
+  async function onRefresh() {
+    mutate();
+  }
+
+  return (
+    <div className="content-scroll">
+      <div className="scroll">
+        <HeadingBox>
+          <Heading
+            color="gradient"
+            css={{
+              marginTop: "$sm",
+              marginBottom: "$sm",
+              textAlign: "center",
+              textTransform: "uppercase",
+            }}
+          >
+            Rooms
+          </Heading>
+          <div className="refresh-icon-container">
+            <IconButton
+              css={{ borderRadius: "100%", span: { path: { fill: "none" } } }}
+              iconSize={{ "@base": "lg", "@md": "xl" }}
+              aria-label="Refresh list of rooms"
+              variant="ghost"
+              icon={<SyncOutline color="inherit" />}
+              onClick={onRefresh}
+              state={isRefreshing ? "loading" : "default"}
+            />
+          </div>
+        </HeadingBox>
+
+        {error ? (
+          <EmptyState css={{ fill: "$text" }}>
+            <Icon size="lg" icon={<SadOutline color="inherit" />} />
+            <Text size="md">
+              An error has occurred. Please try again later.
+            </Text>
+          </EmptyState>
+        ) : isRefreshing ? (
+          <EmptyState>
+            <Text size="md">Refreshing...</Text>
+          </EmptyState>
+        ) : rooms.length < 1 ? (
+          <EmptyState css={{ fill: "$text" }}>
+            <Icon size="lg" icon={<SadOutline color="inherit" />} />
+            <Text size="md">There are currently no available rooms</Text>
+          </EmptyState>
+        ) : (
+          <List>
+            {rooms.length > 0
+              ? rooms.map((r) => {
+                  if (r) {
+                    return (
+                      <ListButton
+                        onClick={() => {
+                          updateState({
+                            type: "select-room",
+                            properties: { room: r },
+                          });
+                        }}
+                        key={r.room}
+                        as="button"
+                        aria-label={`Room: ${r.room}, participants: ${r.participants}`}
+                        noOfParticipants={r.participants}
+                      >
+                        {r.room}
+                      </ListButton>
+                    );
+                  }
+                  return null;
+                })
+              : null}
+          </List>
+        )}
+      </div>
+    </div>
+  );
+}
